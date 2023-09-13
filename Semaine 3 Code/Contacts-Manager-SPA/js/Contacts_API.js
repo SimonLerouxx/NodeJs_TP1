932ed3dd63f7245272246b34f7d0042c19579742
SimonLerouxx <95500169+SimonLerouxx@users.noreply.github.com>
2023-09-13 13:42:06 -0400

    version glitch1

--- Semaine 3 Code/Contacts-Manager-SPA/js/Contacts_API.js
+++ Semaine 3 Code/Contacts-Manager-SPA/js/Contacts_API.js
@@ -1,7 +1,7 @@
 class Contacts_API {
     static API_URL() { 
-        return "https://unique-amusing-confidence.glitch.me/Semaine%203%20Code/contacts.json";
-        //return "http://localhost:5000/api/contacts" 
+        //return "https://unique-amusing-confidence.glitch.me/Semaine%203%20Code/contacts.json";
+        return "http://localhost:5000/api/contacts" 
     };
     static async Get(id = null) {
         return new Promise(resolve => {
